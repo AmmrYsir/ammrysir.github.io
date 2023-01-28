ff46d636ace4e7ec97d3d1c77919470b7d4e2371
Ammar Yasir <ammaryasir03062000@gmail.com>
2023-01-28 14:59:15 +0800

    [feat] AboutMe components

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<section class="h-screen border-b" id="about">
+		<div class="h-full flex flex-col justify-center text-2xl pl-5">
+			<h1 class="mb-0 font-bold text-8xl">
+				Ammar
+				<span class="text-violet-500">Yasir</span>
+			</h1>
+			<div class="mb-5">
+				Contact me through my email -
+				<a class="underline text-violet-500" href="mailto:name@email.com">ammaryasir03062000@gmail.com</a>
+			</div>
+			<p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis
+				for high level overviews. Iterative approaches to corporate strategy foster collaborative
+				thinking to further the overall value proposition.</p>
+			<div class="flex gap-5">
+				<a target="_blank"
+					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
+					href="https://www.linkedin.com/in/ammar-yasir-b619011a9/"><i class="fab fa-linkedin-in"></i></a>
+				<a target="_blank"
+					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
+					href="https://github.com/AmmrYsir"><i class="fab fa-github"></i></a>
+				<a target="_blank"
+					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
+					href="https://twitter.com/AmmarYasirJB"><i class="fab fa-twitter"></i></a>
+				<a target="_blank"
+					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
+					href="https://www.facebook.com/ammar.yasir.03062000/"><i class="fab fa-facebook-f"></i></a>
+			</div>
+		</div>
+	</section>
+</template>
