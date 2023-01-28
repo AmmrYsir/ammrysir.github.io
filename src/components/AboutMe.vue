3b7d973d9f57eb3c8c324528fb4a8cb7b8f38a5b
Ammar Yasir <ammaryasir03062000@gmail.com>
2023-01-28 15:48:20 +0800

    [feat] change introductionary

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-	<section class="h-screen border-b" id="about">
+	<section class="min-h-screen h-screen border-b" id="about">
 		<div class="h-full flex flex-col justify-center text-2xl pl-5">
 			<h1 class="mb-0 font-bold text-8xl">
 				Ammar
@@ -13,9 +13,11 @@
 				Contact me through my email -
 				<a class="underline text-violet-500" href="mailto:name@email.com">ammaryasir03062000@gmail.com</a>
 			</div>
-			<p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis
-				for high level overviews. Iterative approaches to corporate strategy foster collaborative
-				thinking to further the overall value proposition.</p>
+			<p class="lead mb-5">I am somehow experienced web developer with a strong background in the
+				Laravel and Livewire frameworks. With several years of experience in building web applications using
+				Laravel, I have a good understanding of the framework and its features and i confident enough that my
+				experience and expertise make me an excellent candidate for any role that requires strong Laravel and
+				Livewire skills.</p>
 			<div class="flex gap-5">
 				<a target="_blank"
 					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
