60a4214b785f70745f7bc00ee84ff5712cacebbc
Ammar Yasir <ammaryasir03062000@gmail.com>
2023-01-29 09:56:29 +0800

    [feat] updated Experience components

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,32 +3,45 @@
 </script>
 
 <template>
-	<section class="h-screen border-b" id="experience">
-		<div class="h-full flex flex-col justify-center text-2xl pl-5">
-			<h1 class="mb-0 font-bold text-8xl">
-				Ammar
-				<span class="text-violet-500">Yasir</span>
-			</h1>
-			<div class="mb-5">
-				Contact me through my email -
-				<a class="underline text-violet-500" href="mailto:name@email.com">ammaryasir03062000@gmail.com</a>
+	<section class="h-screen" id="experience">
+		<div class="h-full flex flex-col justify-center px-10">
+			<h2 class="mb-5 font-bold text-6xl">EXPERIENCE</h2>
+			<div class="flex justify-between mb-10">
+				<div class="flex-1 text-xl">
+					<h3 class="mb-0 font-semibold text-2xl">SYSTEM ANALYST</h3>
+					<div class="mb-3 text-gray-600">KETARIS ENTERPRISE</div>
+					<p>Bring to the table win-win survival strategies to ensure proactive domination. At the end
+						of the day, going forward, a new normal that has evolved from generation X is on the
+						runway heading towards a streamlined cloud solution. User generated content in real-time
+						will have multiple touchpoints for offshoring.</p>
+				</div>
+				<span class="text-violet-900">December 2022 - Present</span>
 			</div>
-			<p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis
-				for high level overviews. Iterative approaches to corporate strategy foster collaborative
-				thinking to further the overall value proposition.</p>
-			<div class="flex gap-5">
-				<a target="_blank"
-					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
-					href="https://www.linkedin.com/in/ammar-yasir-b619011a9/"><i class="fab fa-linkedin-in"></i></a>
-				<a target="_blank"
-					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
-					href="https://github.com/AmmrYsir"><i class="fab fa-github"></i></a>
-				<a target="_blank"
-					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
-					href="https://twitter.com/AmmarYasirJB"><i class="fab fa-twitter"></i></a>
-				<a target="_blank"
-					class="flex justify-center items-center bg-violet-500 rounded-full text-white h-12 w-12 text-center"
-					href="https://www.facebook.com/ammar.yasir.03062000/"><i class="fab fa-facebook-f"></i></a>
+			<div class="flex justify-between mb-10">
+				<div class="flex-1 text-xl">
+					<h3 class="mb-0 font-semibold text-2xl">JUNIOR DEVOPS ENGINEER</h3>
+					<div class="mb-3 text-gray-600">INFINITE LOGIX SYSTEM PLT</div>
+					<p>Bring to the table win-win survival strategies to ensure proactive domination. At the end
+						of the day, going forward, a new normal that has evolved from generation X is on the
+						runway heading towards a streamlined cloud solution. User generated content in real-time
+						will have multiple touchpoints for offshoring.</p>
+				</div>
+				<span class="text-violet-900">April 2021 - November 2022 (1 year 7 months)</span>
+			</div>
+			<div class="flex justify-between mb-10">
+				<div class="flex-1 text-xl">
+					<h3 class="mb-0 font-semibold text-2xl">INTERN DEVOPS ENGINEER</h3>
+					<div class="mb-3 text-gray-600">INFINITE LOGIX SYSTEM PLT</div>
+					<p>Introduction to industrial experience and workplace. Probation period in development operation
+						start
+						by a few challenges of simple backend project. <br /> "E-GYM" simple backend project challenge
+						created
+						with Laravel that provide CRUD elements with subscription feature. I learned Livewire which
+						known as frontend technologies throughout my probation journey and "E-GYM" received a good
+						feedback. I have been absorbed into real-industry project after few months so that i can
+						experience much more.</p>
+				</div>
+				<span class="text-violet-900">December 2020 - Mac 2021 (3 months)</span>
 			</div>
 		</div>
 	</section>
