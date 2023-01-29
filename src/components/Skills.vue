6dcda1996cf77df03a32243308c6cbf82d7c8084
Ammar Yasir <ammaryasir03062000@gmail.com>
2023-01-29 14:15:38 +0800

    [feat] Skills component init

---
+++ src/components/Skills.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<section class="min-h-screen h-screen border-b" id="skills">
+		<div class="h-full flex flex-col justify-center px-10 text-2xl">
+			<h2 class="mb-5 font-bold text-6xl">SKILLS</h2>
+			<div class="mb-5">
+				<div class="text-gray-600 mb-3">Programming Languages & Tools</div>
+				<ul class="flex gap-5 text-6xl">
+					<li><i class="fab fa-git-square"></i></li>
+					<li><i class="fab fa-js-square"></i></li>
+					<li><i class="fab fa-vuejs"></i></li>
+					<li><i class="fab fa-npm"></i></li>
+					<li><i class="fab fa-node"></i></li>
+					<li><i class="fab fa-php"></i></li>
+					<li><i class="fab fa-laravel"></i></li>
+					<li><i class="fab fa-digital-ocean"></i></li>
+					<li><i class="fab fa-linux"></i></li>
+				</ul>
+			</div>
+			<div class="my-5">Workflow</div>
+			<ul class="fa-ul mb-0">
+				<li>
+					<span class="fa-li"><i class="fas fa-check"></i></span>
+					Mobile-First, Responsive Design
+				</li>
+				<li>
+					<span class="fa-li"><i class="fas fa-check"></i></span>
+					Cross Browser Testing & Debugging
+				</li>
+				<li>
+					<span class="fa-li"><i class="fas fa-check"></i></span>
+					Cross Functional Teams
+				</li>
+				<li>
+					<span class="fa-li"><i class="fas fa-check"></i></span>
+					Agile Development & Scrum
+				</li>
+			</ul>
+		</div>
+	</section>
+</template>
